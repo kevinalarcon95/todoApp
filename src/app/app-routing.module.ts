518eb56e25f824c9e8e6828d0cc79fab5f1dd2df
Kevin Alarcón <57530393+kevinalarcon95@users.noreply.github.com>
2025-03-21 19:49:55 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandingComponent } from './components/branding/branding.component';
+import { AllTasksComponent } from './components/categories/all-tasks/all-tasks.component';
+import { PendingTasksComponent } from './components/categories/pending-tasks/pending-tasks.component';
+import { CompletedTasksComponent } from './components/categories/completed-tasks/completed-tasks.component';
+//import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BrandingComponent },                // Página de bienvenida/instructivo
+  { path: 'home', component: BrandingComponent },            // Página de bienvenida/instructivo
+  { path: 'tareas', component: AllTasksComponent },            // Todas las tareas
+  { path: 'tareas-pendientes', component: PendingTasksComponent }, // Tareas pendientes
+  { path: 'tareas-completadas', component: CompletedTasksComponent }, // Tareas completadas
+  //{ path: 'login', component: LoginComponent },                // Login
+  { path: '**', redirectTo: '' }                               // Ruta comodín, redirige a Branding
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandingComponent } from './components/branding/branding.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AllTasksComponent } from './components/categories/all-tasks/all-tasks.component';
+import { PendingTasksComponent } from './components/categories/pending-tasks/pending-tasks.component';
+import { CompletedTasksComponent } from './components/categories/completed-tasks/completed-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandingComponent,
+    SidebarComponent,
+    AllTasksComponent,
+    PendingTasksComponent,
+    CompletedTasksComponent
   ],
   imports: [
     BrowserModule,
