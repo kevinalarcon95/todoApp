267554de20027a7ff9ff3c6ce7451a37f63a3c04
Kevin Alarcón <57530393+kevinalarcon95@users.noreply.github.com>
2025-03-24 12:19:34 -0500

    feat: Actualiza estilos del sidebar, ajustes en all-task y alertas de tareas
    - Refactoriza estilos del sidebar para mejorar márgenes y presentación.
    - Realiza modificaciones en el componente all-task para optimizar el layout.
    - Agrega mensajes de alerta para confirmar creación, edición y eliminación de tareas.

--- src/app/components/categories/all-tasks/all-tasks.component.ts
+++ src/app/components/categories/all-tasks/all-tasks.component.ts
@@ -1,5 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Task, TaskStatus } from 'src/app/models/task.model';
 import { TaskService } from 'src/app/services/task.service';
 
@@ -10,13 +11,15 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class AllTasksComponent implements OnInit {
   public tasks: Task[] = [];
-  public p: number = 1;
   public isSidebarOpen: boolean = false;
   public selectedTaskIndex: number | null = null;
   public taskForm: FormGroup;
   public taskTitle: string = '';
   public TaskStatus = TaskStatus;
   public isEditingTask: boolean = false;
+  public alertMessage: string = '';
+  public showAlert: boolean = false;
+  public alertType: 'create' | 'edit' | 'delete' = 'create';
 
   constructor(private taskService: TaskService, private fb: FormBuilder) { }
 
@@ -37,8 +40,8 @@ export class AllTasksComponent implements OnInit {
 
   initForm(): FormGroup {
     return this.fb.group({
-      title: [''],
-      description: [''],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(5)]],
       status: [''],
       createdAt: ['']
     });
@@ -61,41 +64,59 @@ export class AllTasksComponent implements OnInit {
   }
 
   saveList(): void {
+    if(this.taskForm.invalid){
+      this.showAlertMessage('Por favor, completa el formulario.');
+      return;
+    }
     const task: Task = this.taskForm.value;
 
     if (this.isEditingTask && this.selectedTaskIndex !== null) {
       this.tasks[this.selectedTaskIndex] = { ...this.tasks[this.selectedTaskIndex], ...task };
       this.isEditingTask = false;
+      this.selectedTaskIndex = null;
+      this.alertType = 'edit';
+      this.showAlertMessage('Tarea editada con éxito.');
     } else {
       this.tasks.unshift(task);
+      this.alertType = 'create';
+      this.showAlertMessage('Tarea creada con éxito.');
     }
-    this.sortTasks();
     this.closeSidebar();
   }
 
-  editTask(task: Task): void {
+  editTask(task: Task, index: number): void {
     this.taskTitle = 'Editar tarea';
     this.isEditingTask = true;
     this.isSidebarOpen = true;
+    this.selectedTaskIndex = index;
     this.taskForm.patchValue({
       ...task,
-      createdAt: task.createdAt || new Date()
+      createdAt: task.createdAt ? formatDate(new Date(task.createdAt), 'yyyy-MM-dd', 'en') : null,
     });
-    this.tasks[this.selectedTaskIndex as number] = task;
   }
 
   deleteTask(index: number): void {
     this.tasks.splice(index, 1);
+    this.alertType = 'delete';
+    this.sortTasks();
+    this.showAlertMessage('Tarea eliminada con éxito.');
   }
 
   toggleTaskDetails(index: number): void {
     this.selectedTaskIndex = this.selectedTaskIndex === index ? null : index;
-    this.editTask(this.tasks[index]);
   }
 
   toggleTaskStatus(index: number): void {
     this.tasks[index].status = this.tasks[index].status === TaskStatus.Pending ? TaskStatus.Completed : TaskStatus.Pending;
     this.sortTasks();
+  }
+
+  private showAlertMessage(message: string): void {
+    this.alertMessage = message;
+    this.showAlert = true;
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 3000);
   }
 
 }
